86fce39afdf330556f547fdae67f98793a71b871
Ben <ben.dalton@madetech.com>
2020-10-04 22:43:44 +0100

    test to return fizz for multiples of 5 passes

--- fizz_buzz_kata/lib/fizz_buzz.rb
+++ fizz_buzz_kata/lib/fizz_buzz.rb
@@ -2,7 +2,7 @@ class FizzBuzz
     def fizz_buzz(num)
         if num == 3 || num % 3 == 0
             "fizz"
-        elsif num == 5
+        elsif num == 5 || num % 5 == 0
             "buzz"
         else
             num.to_s
