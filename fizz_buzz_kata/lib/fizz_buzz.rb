8094fd3dbe41d2a20e611d13dee23401498ad19a
Ben <ben.dalton@madetech.com>
2020-10-07 08:41:38 +0100

    small refactor on if statements

--- fizz_buzz_kata/lib/fizz_buzz.rb
+++ fizz_buzz_kata/lib/fizz_buzz.rb
@@ -2,9 +2,9 @@ class FizzBuzz
     def fizz_buzz(num)
         if num % 3 == 0 && num % 5 == 0
             "fizzbuzz"        
-        elsif num == 3 || num % 3 == 0
+        elsif num % 3 == 0
             "fizz"
-        elsif num == 5 || num % 5 == 0
+        elsif num % 5 == 0
             "buzz"
         else
             num.to_s
