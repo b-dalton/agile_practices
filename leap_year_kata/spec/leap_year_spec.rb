c3c84a4e0498f8293a2fd3f754813368f52140d7
Ben <ben.dalton@madetech.com>
2020-10-08 15:27:42 +0100

    re-worded test name

--- leap_year_kata/spec/leap_year_spec.rb
+++ leap_year_kata/spec/leap_year_spec.rb
@@ -3,12 +3,12 @@ require 'leap_year'
 describe 'LeapYear' do
     context 'when given a year' do 
         it 'returns true or false depending on whether the input is a leap year or not' do
-        # Arrange
-        leap_year = LeapYear.new
-        # Act
-        year_check = leap_year.is_leap_year(2014)
-        # Assert
-        expect(year_check).to be false
+            # Arrange
+            leap_year = LeapYear.new
+            # Act
+            year_check = leap_year.is_leap_year(2014)
+            # Assert
+            expect(year_check).to be false
         end
     end
 end
