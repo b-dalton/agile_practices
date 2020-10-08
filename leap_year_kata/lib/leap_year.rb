4d285589ced45879a99453bd8dbadbb9dad68b9a
Ben <ben.dalton@madetech.com>
2020-10-08 16:25:15 +0100

    re-wrote first test

--- leap_year_kata/lib/leap_year.rb
+++ leap_year_kata/lib/leap_year.rb
@@ -1,11 +1,7 @@
 class LeapYear
     def is_leap_year(year)
-<<<<<<< HEAD
-        if year == 2020
+        if year == 2000
             true
         end
-=======
-        false
->>>>>>> c3c84a4e0498f8293a2fd3f754813368f52140d7
     end
 end
--- leap_year_kata/spec/leap_year_spec.rb
+++ leap_year_kata/spec/leap_year_spec.rb
@@ -1,25 +1,14 @@
 require 'leap_year'
 
 describe 'LeapYear' do
-<<<<<<< HEAD
-    context 'when given a year that is a leap year' do 
+    context 'when given centurial year 2000 that is divisible by 400' do 
         it 'returns true' do
             # Arrange
             leap_year = LeapYear.new
             # Act
-            year_check = leap_year.is_leap_year(2020)
+            year_check = leap_year.is_leap_year(2000)
             # Assert
             expect(year_check).to be true
-=======
-    context 'when given a year' do 
-        it 'returns true or false depending on whether the input is a leap year or not' do
-            # Arrange
-            leap_year = LeapYear.new
-            # Act
-            year_check = leap_year.is_leap_year(2014)
-            # Assert
-            expect(year_check).to be false
->>>>>>> c3c84a4e0498f8293a2fd3f754813368f52140d7
         end
     end
 end
